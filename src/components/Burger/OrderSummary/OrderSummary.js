f948fb4954ac7ed606162da856d502a747b35f08
intelligentCoding <31515792+intelligentCoding@users.noreply.github.com>
2019-01-12 10:21:44 -0500

    Order Summary Total Price implemented

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,6 +16,10 @@ const orderSummary = props => {
       <h3>Your Order </h3>
       <p> A delicious burger with the following ingredients: </p>
       <ul> {ingredientsSummary}</ul>
+      <p>
+        {" "}
+        <strong>Total Price: {props.price.toFixed(2)} </strong>
+      </p>
       <p>Continue to check out</p>
       <Button btnType="Danger" clicked={props.purcheseCancelled}>
         CANCEL
